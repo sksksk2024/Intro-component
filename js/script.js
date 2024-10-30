7acb708181753adbea7eb4531b76656467530951
sksksk2024 <157216629+sksksk2024@users.noreply.github.com>
2024-10-30 08:08:36 +0000

    Finished the Project

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#myForm');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent the form from submitting automatically
+
+    // Check if all inputs are valid
+    if (form.checkValidity()) {
+      try {
+        alert('Thank you for your time!');
+      } catch (e) {
+        console.log('ERROR!');
+      }
+    }
+  });
+});
